e5dfaa53c33db9caa22f04a7782373018ae89f98
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-03-06 18:25:24 +0000

    Ex 2.7 Completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,6 +8,10 @@ const App = () => {
 
   function handleFormSubmission(event) {
     event.preventDefault()
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return;
+    }
     const personObject = {name: newName}
     setPersons(persons.concat(personObject))
   }
