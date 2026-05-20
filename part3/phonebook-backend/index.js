0667cc17eb0809bb80a4622838a668bf2c5f856c
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-05-20 11:50:02 +0000

    Exercise 3.6 Completed

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>persons has info for ${persons.length} people</p>
+    <p>${new Date()}</p>  
+  `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name field is empty'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number field is empty'
+    })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({
+      error: 'name is not unique'
+    })
+  }
+
+  const newPerson = body
+  newPerson.id = String(Math.floor(Math.random() * 10000000))
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
+})
+
+PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
